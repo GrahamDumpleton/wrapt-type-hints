9e0994ee225b804f51019630db7254ce5158b08d
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2026-01-16 15:28:38 +1100

    Add error checks for decorator methods.

---
+++ tests/test_method_incorrect_return_type.py
@@ -0,0 +1,27 @@
+from decorators.pass_through import pass_through
+
+
+class Test:
+    @pass_through
+    def function_im(self, a: int, b: int) -> int:
+        return a + b
+    
+    @pass_through
+    @classmethod
+    def function_cm(cls, a: int, b: int) -> int:
+        return a + b
+    
+    @pass_through
+    @staticmethod
+    def function_sm(a: int, b: int) -> int:
+        return a + b
+
+instance = Test()
+
+result_im: str = instance.function_im(2, 3)
+
+result_cm_1: str = Test.function_cm(2, 3)
+result_cm_2: str = instance.function_cm(2, 3)
+
+result_sm_1: str = Test.function_sm(2, 3)
+result_sm_2: str = instance.function_sm(2, 3)
